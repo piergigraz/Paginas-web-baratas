f25e2d9d1c65ad84eb579fc28d29cf58cb6a739c
Pier <piergigraz@gmail.com>
2019-11-29 21:08:19 +0100

    con APP per menu mobile

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
-let abba = 9;
+const p = document.querySelector('#mobile-menu');
+const ul = document.querySelector('.nav-links');
 
-console.log(abba);
 
-console.log('prova');
+const mobileMenu = () => {
+ul.classList.toggle('hide');
+p.classList.toggle('rotated');
+}
+
+p.addEventListener('click', mobileMenu)
